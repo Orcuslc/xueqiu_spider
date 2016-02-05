73f8e35c20a71d86dc3d29b5171451156f899efd
orcuslc <orcuslc@hotmail.com>
2016-02-06 00:01:44 +0800

    get stock basics

---
+++ get_stock_basics.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import tushare as ts 
+import sqlalchemy as sqlc
+
+route = 'sqlite:///data/stock.db'
+engine = sqlc.create_engine(route)
+
+def pull_stock_basics():
+	stock_basics = pd.read_sql('SELECT * from stock_basics', engine)
+	if stock_basics.empty == True:
+		stock_basics = ts.get_stock_basics()
+		stock_basics.to_sql('stock_basics', engine, if_exists = 'replace')
+	return stock_basics
+
+
+if __name__ == '__main__':
+	stock_basics = pull_stock_basics()
+	print(stock_basics)
