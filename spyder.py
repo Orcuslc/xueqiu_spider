c4f7a6112f48bef804fde0c3f6768e5fafb2f239
orcuslc <orcuslc@hotmail.com>
2016-03-01 21:50:38 +0800

    save to sql added

--- spyder.py
+++ spyder.py
@@ -3,9 +3,9 @@ import re
 # from datetime import datetime as dt
 import time
 # import sqlite3
-import pymongo as mg 
-import multiprocessing as mtp
-import threading as tr 
+import pymongo as pmg 
+# import multiprocessing as mtp
+# import threading as tr 
 
 class comment_spyder:
 	'''
@@ -26,9 +26,9 @@ class comment_spyder:
 			'Cookie': 'Hm_lvt_1db88642e346389874251b5a1eded6e3=1455272909,1456817053,1456817184; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1456831074; s=1mkm1low5q; xq_a_token=fb836770b2d5ca88e71608d0c21327a306073355; xq_r_token=7eeee1f3170459bb8bcc6aa066e53cc299167093; __utma=1.503405128.1456830680.1456830680.1456833100.2; __utmc=1; __utmz=1.1456830680.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); webp=0; __utmb=1.3.10.1456833100; __utmt=1',
 			'Connection': 'keep-alive'
 			}
-		self.today = dt.now()
+		# self.today = dt.now()
 		self.url = self._get_url()
-		self.timestamp = None
+		self.timestamp_list = []
 		self.headers['Referer'] = self.url
 
 	def _get_url(self):
@@ -44,11 +44,11 @@ class comment_spyder:
 		url = 'http://xueqiu.com/statuses/search.json?count=10&comment=0&symbol=' \
 			+ self.url[-8:] + '&h1=0&source=all&sort=alpha&page=' + str(count)
 		page = requests.get(url, headers = self.headers)
-		comments = re.findall(r'"created_at":[0-9]*,', page.text)
-		if comments == []:
+		comment = re.findall(r'"created_at":[0-9]*,', page.text)
+		if comment == []:
 			return [], False
 		timestamp = []
-		for com in comments:
+		for com in comment:
 			'''
 			The result should be like '"created_at":1450698976000,'
 			'''
@@ -72,15 +72,32 @@ class comment_spyder:
 	def get_comment_time(self):
 		count = 1
 		flag = True
-		timestamp_list = []
 		while flag == True:
 			timestamp, flag = self._fetch_timestamp(count, flag)
 			if flag == True:
-				timestamp_list.extend(list(map(self._handle_timestamp, timestamp)))
+				self.timestamp_list.extend(list(map(self._handle_timestamp, timestamp)))
 			count += 1
-		return timestamp_list
+		# return timestamp_list
+
+	def save_to_sql(self):
+		client = pmg.MongoClient('localhost', 27017)
+		db = client['stock_database']
+		collection = db['comment']
+		# post_list = []
+		for timestamp in self.timestamp_list:
+			post = {
+			'code' : self.code,
+			'date' : timestamp
+			}
+			# post_list.append(post)
+			db.collection.insert_one(post)
+
+	def incremental_update(self):
+		
+
 
 if __name__ == '__main__':
 	sp = comment_spyder('000917')
 	l = sp.get_comment_time()
 	print(l)
+	sp.save_to_sql()
