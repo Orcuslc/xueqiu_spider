ce765b759b1c271d5608a7a0ebe3f499832d2ab1
Orcuslc <orcuslc@hotmail.com>
2016-05-15 16:52:06 +0000

    A little bug in follower spyder

--- new_spyder.py
+++ new_spyder.py
@@ -19,46 +19,48 @@ class comment_spyder:
 			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0',
 			'Accept': 'application/json, text/javascript, */*; q=0.01',
 			'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-			# Accept-Encoding: gzip, deflate
 			'Cache-Control': 'no-cache',
 			'X-Requested-With': 'XMLHttpRequest',
 			'Connection': 'keep-alive'
 			}
 		self._get_url()
 		self._get_cookies() # get cookies before getting data in order to prevent that the request would be refused;
-		self._count = 0 # the number of data;
-		self.data = pd.DataFrame() 
+		# self._count = 0 # the number of data;s
+		self.data = pd.DataFrame()
 		self._conn = conn # SQLAlchemy Engine
+		self._headers["Referer"] = self._url
 
 	def _get_cookies(self):
 		'''Get the cookies for xueqiu.com'''
-		url = 'http://xueqiu.com'
-		r = requests.get(url, headers = self._headers)
+		url = 'https://xueqiu.com'
+		r = requests.get(url, headers = self._headers, verify = False)
 		self._cookies = r.cookies
 
 	def _get_url(self):
 		'''Get url for the stock'''
 		if self._code[0] in ['3', '0']:
-			url = 'http://xueqiu.com/S/' + 'SZ' + self._code
+			url = 'https://xueqiu.com/S/' + 'SZ' + self._code
 		elif self._code[0] == '6':
-			url = 'http://xueqiu.com/S/' + 'SH' + self._code
+			url = 'https://xueqiu.com/S/' + 'SH' + self._code
 		else:
 			raise BaseException('Failed to get url for the stock %s, Please check again' %self._code)
 		self._url = url
 
 	def _get_data(self):
 		'''Get the comments data with the index of page; The range of index should be within 1 and 100;'''
-		for index in range(1, 100):
-			print(self.code, 'comment', index)
+		for index in range(1, 3):
+			print(self._code, 'comment', index)
 			time.sleep(0.1)
 			url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol=' + self._url[-8:] + '&hl=0&source=all&sort=time&page=' + str(index)
 			try:
-				text = requests.get(url, headers = self._headers, cookies = self._cookies).text
+				text = requests.get(url, headers = self._headers, cookies = self._cookies, verify = False).text
 			except requests.exceptions.ConnectionError:
 				break
 			record_list = json.loads(text)["list"]
 			for record in record_list:
 				record['time'] = self._handle_time(record['created_at'])
+				for key in record['user']:
+					record[str('user_'+key)] = record['user'][key]
 				record['user'] = str(record['user'])
 				record['retweeted_status'] = str(record['retweeted_status'])
 				record = pd.Series(record)
@@ -79,13 +81,78 @@ class comment_spyder:
 		self.data.to_sql('stock_comments', self._conn, if_exists = 'replace')
 
 	def run(self):
-		print('%s Started;'%self.code)
+		print('%s Started;'%self._code)
 		self._get_data()
 		# self.data = self.data.fillna(0.0)
 		self._save_to_database()
-		print('%s Ended;'%self.code)
+		print('%s Ended;'%self._code)
 
+class follower_spyder(comment_spyder):
+	def __init__(self, code, conn):
+		super().__init__(code, conn)
+		self.trading_data = pd.DataFrame()
+
+	def _get_data(self):
+		# self._headers["referer"] = self._url
+		# self._cookies = requests.get(self._url, headers = self._headers, cookies = self._cookies, verify = False).cookies
+		for index in range(1, 2):
+			print(self._code, 'follower', index)
+			time.sleep(0.1)
+			url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol=' + self._url[-8:] + '&hl=0&source=trans&page=' + str(index)
+			try:
+				r = requests.get(url, headers = self._headers, cookies = self._cookies, verify = False)
+				if r.status_code == 301:
+					r = requests.get(url, headers = self._headers, cookies = self._cookies, verify = False)
+				else:
+					text = r.text
+			except requests.exceptions.ConnectionError:
+				break
+			record_list = json.loads(text)["list"]
+			for record in record_list:
+				record['time'] = self._handle_time(record['created_at'])
+				for key in record['user']:
+					record[str('user_'+key)] = record['user'][key]
+				record['user'] = str(record['user'])
+				record['retweeted_status'] = str(record['retweeted_status'])
+				a = record['text']
+				if a[0] not in [i for i in range(10)]:
+					try:
+						price = re.findall(r'当前价[0-9]+\.[0-9]+', a)[0][3:]
+						print(price)
+					except BaseException:
+						price = 0.0
+					record["follow_price"] = str(price)
+					record["sold_price"] = record["bought_price"] = 0.0
+					record = pd.Series(record)
+					record.name = self._code
+					self.data.append(record)
+				else:
+					price = re.findall(r'￥[0-9]+.[0-9]+买入', a)
+					if price != []:
+						record["bought_price"] = price[0][1:-2]
+						record["sold_price"] = 0.0
+					else:
+						price = re.findall(r'￥[0-9]+.[0-9]+卖出', a)
+						if price != []:
+							record["sold_price"] = price[0][1:-2]
+							record["bought_price"] = 0.0
+						else:
+							price = 0.0
+							record["sold_price"] = record["bought_price"] = price
+					
+					record["follow_price"] = 0.0
+					record = pd.Series(record)
+					record.name = self._code
+					self.trading_data.append(record)
+
+	def _save_to_database(self):
+		self.data.to_sql('stock_followers', self._conn, if_exists = 'replace')
+		self.trading_data.to_sql('stock_tradings', self._conn, if_exists= 'replace')
+
+	def run(self):
+		self._get_data()
+		self._save_to_database()
 
 if __name__ == '__main__':
-	c = comment_spyder('000001', conn)
+	c = follower_spyder('002618', conn)
 	c.run()
